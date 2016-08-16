6f8d8379c1ae9b42a95b2b26a1c59e736670576c
ashin <dev@ashinstarr.com>
2016-08-16 22:19:00 +1000

    sorted out wp prod flag

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,27 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-process.env.NODE_ENV = 'production';
+var isProduction = function() {
+  return process.env.NODE_ENV === 'production';
+}
+
+var plugins = [
+    new ExtractTextPlugin("[name].css")
+  ];
+  if(!isProduction()) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  } else {
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        sourceMap: false,
+        compress: {
+            drop_console: true
+        },
+        mangle: {
+            except: ['$super', '$', 'exports', 'require', '$q', '$ocLazyLoad']
+        }
+    }));
+  }
 
 module.exports = {
   entry: [
@@ -14,7 +34,7 @@ module.exports = {
        exclude: /node_modules/,
        loaders: ['babel']
      },
-     { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=' + (process.env.NODE_ENV !== 'production' ? '[name]__[local]___[hash:base64:7]' : '[hash:base64:7]') + '!postcss-loader!stylus-loader') },
+     { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=' + (isProduction() ? '[hash:base64:7]' : '[name]__[local]___[hash:base64:7]') + '!postcss-loader!stylus-loader') },
      { test: /\.png$/, loader: "url-loader?limit=100000" },
      { test: /\.jpg$/, loader: "file-loader" }
    ]
@@ -35,18 +55,5 @@ module.exports = {
   devServer: {
     contentBase: './dist'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new ExtractTextPlugin("[name].css"),
-    new webpack.optimize.UglifyJsPlugin({
-        minimize: true,
-        sourceMap: false,
-        compress: {
-            drop_console: true
-        },
-        mangle: {
-            except: ['$super', '$', 'exports', 'require', '$q', '$ocLazyLoad']
-        }
-    })
-  ]
+  plugins: plugins
 };
